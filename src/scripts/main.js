231b5019dbae8a52b1d806b1ecad7f81e1953394
Bohdan Leonets <leonetsbogdan@gmail.com>
2020-11-05 21:42:21 +0200

    svgs and ability to add movie to favorite movies were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,49 +2,54 @@
 
 const url = 'http://my-json-server.typicode.com/moviedb-tech/movies/list';
 
-const options = {
-  method: 'GET',
-};
-
-let moviesFromServer;
+const moviesFromServer = [];
 const movieFromServer = [];
+let favoritMovies = [];
 
 const root = document.getElementById('root');
 const moviesList = root.querySelector('.movies__list');
+const moviesItem = root.querySelectorAll('.movies__name');
 const favoriteList = root.querySelector('.favorite__list');
+const favorite = root.querySelector('.movies__addFavorite');
 
-// console.log(moviesList);
+// console.log(favoritMovies);
 
-// const moviesFromServer = fetch(url)
-//   .then(response => response.json())
-//   .then(data => movies.push(...data));
+moviesList.addEventListener('click', (event) => {
+  const item = event.target;
+  const moviesItem = item.closest('.movies__item');
+  const moviesName = moviesItem.querySelector('.movies__name');
 
-// getMovies2();
+  if (!item.classList.contains('movies__addFavorite')) {
+    return;
+  }
 
-// async function getMovies2() {
-//   const response = await fetch(url);
+  // if (favoritMovies.includes(moviesName.innerText)) {
+  //   return;
+  // }
 
-//   return response.json();
-// }
+  // favoritMovies = [];
+  favoritMovies.push(moviesName.innerText);
+  initFavoriteMovies(favoritMovies);
 
-// console.log(getMovies2());
+  console.log(item);
+  console.log(moviesItem);
+  console.log(moviesName.innerText);
+  console.log(favoritMovies);
+});
 
 const getMovies = () => {
-  return fetch(url, options)
-    .then((response) => {
-      return response.json();
-    })
+  return fetch(url)
+    .then(response => response.json())
     .then(function (data) {
-      return data;
+      moviesFromServer.push(...data);
 
-      // moviesFromServer = data;
-      // console.log(moviesFromServer, data);
-    } /* moviesFromServer.push(...data) */);
+      initMovies(moviesFromServer);
+
+      return data;
+    });
 };
 
-let a = getMovies();
-
-console.log(a);
+getMovies();
 
 const getMovie = (id) => {
   return fetch(`${url}/${id}`)
@@ -55,16 +60,31 @@ const getMovie = (id) => {
 getMovie(3);
 
 console.log(moviesFromServer);
-console.log(movieFromServer);
+// console.log(movieFromServer);
 
-initMovies(a);
+// initMovies(moviesFromServer);
 
 function initMovies(movies) {
   for (const movie of movies) {
-    moviesList.insertAdjacentHTML('beforebegin', `
+    moviesList.insertAdjacentHTML('beforeend', `
       <div class="movies__item">
-        <div>${movie.name}</div>
+        <img class="movies__image" src=${movie.img} alt="movie image"/>
+        <div class="movies__name">${movie.name}</div>
+        <div class="movies__year">${movie.year}</div>
+        <span class="movies__addFavorite" alt="star" id=${movie.id}></span>
       </div>
     `);
   }
 };
+
+// initFavoriteMovies(favoritMovies);
+
+function initFavoriteMovies(movies) {
+  for (const name of movies) {
+    favoriteList.insertAdjacentHTML('beforeend', `
+      <span class="favorite__before"></span>
+      <span class="favorite__after"></span>
+      <li class="favorite__item">${name}</li>
+    `);
+  }
+}
